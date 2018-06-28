2d7f6b5eb931f21c53d96f3e08fe4a455316c23d
Ryan Fadholi <rynfdhl@gmail.com>
2018-06-28 14:10:28 +0700

    Added custom string representations to tagged statement and tagged token

--- taggedstatement.py
+++ taggedstatement.py
@@ -4,6 +4,22 @@ class TaggedStatement:
     def __init__(self, tagged_tokens = []):
         #Ensure copy
         self.tagged_tokens = copy.copy(tagged_tokens)
+        self.statement_type = "None"
 
     def __len__(self):
         return len(self.tagged_tokens)
+
+    def __str__(self):
+        result = ""
+        result += f"Statement Type: {self.statement_type}\n"
+        result += "Tokens: "
+
+        comma_flag = False
+        for token in self.tagged_tokens:
+            if comma_flag:
+                result  += ", "
+            result += str(token)
+            comma_flag = True
+            
+        return result
+
--- taggedtoken.py
+++ taggedtoken.py
@@ -1,8 +1,7 @@
 class TaggedToken:
     def __init__(self, tag, token):
-        
         self.tag = tag
         self.token = token
 
-    def __contains__(self, item):
-        pass
+    def __str__(self):
+        return f"{self.tag} - {repr(self.token)}"
