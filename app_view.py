b5efb4586b029866b4155c0723892be17eff7710
Ryan Fadholi <rynfdhl@gmail.com>
2018-12-01 11:09:21 +0700

    Add dedicate label for each translation category

--- app_view.py
+++ app_view.py
@@ -12,6 +12,25 @@ OPEN_BUTTON_LABEL = "Open"
 SAVE_BUTTON_LABEL = "Save"
 
 BASELABEL_LINE_COUNT = "Statement count:"
+BASELABEL_DECL = "Deklarasi Variabel/Fungsi\t:"
+BASELABEL_INIT = "Inisialisasi/Inisiasi\t:"
+BASELABEL_FUNC = "Pemanggilan Fungsi\t:"
+BASELABEL_IO = "Masukan/Keluaran\t:"
+BASELABEL_COND = "Kondisional\t\t:"
+BASELABEL_LOOP = "Pengulangan\t\t:"
+BASELABEL_COMMENT = "Komentar\t\t:"
+BASELABEL_ETC = "Lain-lain\t\t:"
+
+BASELABEL_ORDER = [BASELABEL_LINE_COUNT, 
+                BASELABEL_LINE_COUNT,
+                BASELABEL_DECL,
+                BASELABEL_INIT,
+                BASELABEL_FUNC,
+                BASELABEL_IO,
+                BASELABEL_COND,
+                BASELABEL_LOOP,
+                BASELABEL_COMMENT,
+                BASELABEL_ETC]
 
 TEXTFIELD_WIDTH = 450
 TEXTFIELD_HEIGHT = 600
@@ -52,8 +71,30 @@ class C2jsView(QMainWindow):
         self.result_text.setLineWrapMode(QPlainTextEdit.NoWrap)
         self.details_text.setLineWrapMode(QPlainTextEdit.NoWrap)
 
-        self.source_count = QLabel(f"{BASELABEL_LINE_COUNT} -")
-        self.result_count = QLabel(f"{BASELABEL_LINE_COUNT} -")
+        self.source_count = QLabel("")
+        self.result_count = QLabel("") 
+        self.decl_count = QLabel("")
+        self.init_count = QLabel("")
+        self.func_count = QLabel("")
+        self.io_count = QLabel("")
+        self.cond_count = QLabel("")
+        self.loop_count = QLabel("")
+        self.comment_count = QLabel("")
+        self.etc_count = QLabel("")
+
+        self.count_order = [self.source_count,
+            self.result_count,
+            self.decl_count,
+            self.init_count,
+            self.func_count,
+            self.io_count,
+            self.cond_count,
+            self.loop_count,
+            self.comment_count,
+            self.etc_count]
+
+        for label, text in zip(self.count_order, BASELABEL_ORDER):
+            label.setText(f"{text} -")
 
         #Setup the buttons, disable the save_button first
         self._open_button.clicked.connect(self.open)
@@ -74,7 +115,7 @@ class C2jsView(QMainWindow):
         linecountrow.addStretch(1)
         linecountrow.addWidget(self.result_count)
         linecountrow.addStretch(1)
-        linecountrow.setContentsMargins(0,0,0,40)
+        linecountrow.setContentsMargins(0,0,0,25)
         detailsrow.addWidget(self.details_text)
 
         #Stack both rows, texts below buttons
@@ -107,8 +148,8 @@ class C2jsView(QMainWindow):
                 data = output.read()
                 self.source_text.setPlainText(data)
 
-            self.result_count.setText(f"Statement count: <b>{hasil[0]}</b>")
-            self.source_count.setText(f"Statement count: <b>{hasil[0]}</b>")
+            for label, text, count in zip(self.count_order, BASELABEL_ORDER, hasil):
+                label.setText(f"{text} <b>{count}</b>")
             
             details_text = ""
             details_text += f"Deklarasi Variabel/Fungsi\t: {hasil[2]}\n"
